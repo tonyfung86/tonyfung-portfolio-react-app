5bc464c7535ae36b4eb0c315afd4572b5775edab
tonyfung86 <tonyfung86.gmail.com>
2023-09-03 22:17:46 -0400

    Rename the repo and workflow folder to workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({  
   plugins: [react()],
-  base: "/TonyFung-Portfolio-React-App/",
+  base: "/tonyfung-portfolio-react-app/",
 })
