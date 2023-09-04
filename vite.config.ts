a29ff2d4813b880903375019f247dafb1fdf8ecc
tonyfung86 <tonyfung86.gmail.com>
2023-09-03 22:01:08 -0400

    Redo host Vite React App on Github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/tonyfung86.github.io",
+export default defineConfig({  
   plugins: [react()],
+  base: "/TonyFung-Portfolio-React-App/",
 })
