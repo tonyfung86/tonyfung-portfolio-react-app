457b20c8b95c114055c3e86e793d29d65b7c6179
tonyfung86 <tonyfung86.gmail.com>
2023-09-15 00:09:29 -0400

    Add ParallaxLayer and Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-//import Message from "./Message";
-// import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
-// import Button from "./components/Button";
-// import { useState } from "react";
+import TopNav from "./components/TopNav";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Container from "react-bootstrap/Container";
 import top from "./img/top.jpg";
-// import middle from "./img/middle.jpg";
 import bottom from "./img/bottom.jpg";
-//import Row from "react-bootstrap/Row";
-//import Col from "react-bootstrap/Col";
+import { Nav } from "react-bootstrap";
 
-let items = ["US", "JP", "HK", "TW", "UK", "CA"];
+let items = ["JavaScript", "React", "HTML", "jQuery", "XML"];
 
 const handleSelectItem = (item: string) => {
   console.log(item);
@@ -21,48 +16,57 @@ function App() {
   // const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
-    <div>
+    <>
       <Parallax pages={3}>
         <ParallaxLayer
-          style={{ backgroundImage: `url(${top})`, backgroundSize: "cover" }}
-        >
+          offset={0}
+          speed={0}
+          factor={3}
+          style={{
+            backgroundImage: `url(${top})`,
+            backgroundSize: "cover",
+          }}
+        ></ParallaxLayer>
+        <TopNav />
+        <ParallaxLayer offset={0.3} speed={1}>
           <Container>
-            <h1>This is the top of the page</h1>
+            <h1>Hi,</h1>
+            <h1>I'm Tony Fung</h1>
+            <h1>Full stack developer</h1>
           </Container>
         </ParallaxLayer>
+
         <ParallaxLayer
-          offset={0.9}
-          speed={0.1}
-          style={{ backgroundImage: `url(${bottom})`, backgroundSize: "cover" }}
-        >
-          <Container>
-            <ListGroup
-              items={items}
-              heading={"List 1"}
-              onSelectItem={handleSelectItem}
-            />
-          </Container>
-        </ParallaxLayer>
-        <ParallaxLayer
-          offset={1.7}
+          offset={0.7}
           speed={1}
-          style={{ backgroundImage: `url(${top})` }}
+          //style={{ backgroundImage: `url(${bottom})`, backgroundSize: "cover" }}
+        >
+          <Container>
+            <h1>About me</h1>
+            <p>I'm an independent full stack developer</p>
+          </Container>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1.9}
+          speed={1}
+          //style={{ backgroundImage: `url(${bottom})`, backgroundSize: "cover" }}
         >
           <Container>
             <ListGroup
               items={items}
-              heading={"List 2"}
+              heading={"Skills"}
               onSelectItem={handleSelectItem}
             />
           </Container>
         </ParallaxLayer>
         <ParallaxLayer offset={2.5} speed={0.1}>
           <Container>
-            <h2>This is bottom</h2>
+            <h2>Contact me</h2>
           </Container>
         </ParallaxLayer>
       </Parallax>
-    </div>
+    </>
   );
 }
 
