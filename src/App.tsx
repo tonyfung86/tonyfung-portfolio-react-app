0682acb6ed12a4a3321f05e78d77c557fcf1b899
tonyfung86 <tonyfung86.gmail.com>
2023-09-18 18:08:33 -0400

    Adjust Navbar and first ParallaxLayer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import ListGroup from "./components/ListGroup";
 import TopNav from "./components/TopNav";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import top from "./img/top.jpg";
+import programmer from "./img/programmer.png";
 
 let items = ["JavaScript", "React", "HTML", "jQuery", "XML"];
 
@@ -28,20 +32,33 @@ function App() {
         <TopNav />
         <ParallaxLayer offset={0.3} speed={1}>
           <Container>
-            <h1>Hi,</h1>
-            <h1>I'm Tony Fung</h1>
-            <h1>Full stack developer</h1>
+            <Row className="ver-center">
+              <Col md={7}>
+                <span className="font-style1">
+                  Hi,
+                  <br /> I'm <span className="gradient-blue">Tony Fung</span>
+                  <br />
+                  <span className="font-italic">Full stack developer</span>
+                  <h4>Turning Code into Solutions</h4>
+                </span>
+              </Col>
+              <Col className="mobile-hide" md={5}>
+                <Image className="icon" src={programmer} />
+              </Col>
+            </Row>
           </Container>
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={0.7}
+          offset={1}
           speed={1}
           //style={{ backgroundImage: `url(${bottom})`, backgroundSize: "cover" }}
         >
           <Container>
             <h1>About me</h1>
-            <p>I'm an independent full stack developer</p>
+            <p>
+              I'm an independent full stack developer with 2.5 year experience.
+            </p>
           </Container>
         </ParallaxLayer>
 
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,29 +1,22 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function BasicExample() {
   return (
     <Navbar id="navbar" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Tony Fung</Navbar.Brand>
+        <Navbar.Brand href="#home" className="pe-5">
+          <span className="gradient-blue fw-bold">Tony Fung</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#link1">Link1</Nav.Link>
-            <Nav.Link href="#link2">Link2</Nav.Link>
-            <NavDropdown title="Other" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#works">Work</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
